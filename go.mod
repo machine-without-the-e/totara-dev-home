a3af2fcd39d50a219c5d5dd23a3dfc308af9ebea
Aaron Machin <aljd.machin@gmail.com>
2024-08-30 21:15:13 +1200

    wip

--- go.mod
+++ go.mod
@@ -4,6 +4,11 @@ go 1.23.0
 
 require (
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
+	github.com/jackc/pgx/v5 v5.6.0 // indirect
 	github.com/spf13/cobra v1.8.1 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
+	golang.org/x/crypto v0.17.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
 )
--- internal/database/pgsql.go
+++ internal/database/pgsql.go
@@ -1,8 +1,41 @@
 package database
 
+import (
+  "context"
+  "github.com/jackc/pgx/v5"
+  "fmt"
+  "os"
+)
+
 type Pgsql struct {
   config DatabaseConfig
   connection Connection
+}
+
+func (db *Pgsql) connect() {
+  conn, err := pgx.Connect(
+    context.Background(), 
+    fmt.Sprintf("postgres://%s:%s@%s",
+      db.config.User,
+      db.config.Pass,
+      db.config.Host,
+    ),
+  )
+    
+if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
+		os.Exit(1)
+	}
+defer conn.Close(context.Background())
+
+var id int
+	err = conn.QueryRow(context.Background(), "select id from public.ttr_course").Scan(&id)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "QueryRow failed: %v\n", err)
+		os.Exit(1)
+	}
+
+  fmt.Println(id)
 }
 
 func (db *Pgsql) getConnection() Connection {
